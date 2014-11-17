ba1d7be39fc8d6c1012ca0e70d10d7fa3d7aad03
Oksana <oksana.semeshkina@stud.hs-mannheim.de>
2014-11-17 15:02:19 +0100

    Alle methoden eingefügt, noch problem mit java.util.funktion

--- src/ws2014/tpe/gruppe_1415349_1410206/uebung3/binaer_hash_baum/AssoziativesArray.java
+++ src/ws2014/tpe/gruppe_1415349_1410206/uebung3/binaer_hash_baum/AssoziativesArray.java
@@ -1,5 +1,33 @@
 package ws2014.tpe.gruppe_1415349_1410206.uebung3.binaer_hash_baum;
 
-public interface AssoziativesArray {
+//import java.util.function.BiConsumer;
+//import java.util.function.BiFunction;
 
+public interface AssoziativesArray<B, T> {
+
+	public void clear();
+
+	public boolean containsValue(T wert);
+
+	public boolean containsKey(B schluessel);
+
+	public T get(B schluessel);
+
+	public boolean isEmpty();
+
+	public void put(B schluessel, T wert);
+
+//	public void putAll(Baum.Knoten element);
+
+	public T remove(B schluessel);
+
+	public int size();
+
+	public void update(B schluessel, T wert);
+
+//	public void forEach(BiConsumer bi);
+
+	public void extractAll(Baum<?,?> baum);
+
+//	public Baum map(BiFunction funk);
 }
