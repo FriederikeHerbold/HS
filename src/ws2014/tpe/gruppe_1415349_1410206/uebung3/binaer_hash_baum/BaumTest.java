86e70fea49293a44e3b8c86aa211eb383c6e4cea
Oksana <oksana.semeshkina@stud.hs-mannheim.de>
2014-11-20 23:22:47 +0100

    Zweiter Test für Baum 1."put" wurden in contaisKey und containsValue hinzugefügt. 2.Reihenfolge bei to String war falsh? ich hab sie geändert. 3. Testcase in isEmpty, warum falsch?

---
+++ src/ws2014/tpe/gruppe_1415349_1410206/uebung3/binaer_hash_baum/BaumTest.java
@@ -0,0 +1,126 @@
+package ws2014.tpe.gruppe_1415349_1410206.uebung3.binaer_hash_baum;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BaumTest {
+
+	AssoziativesArray liste = new Baum("a", 4);
+	AssoziativesArray liste2 = new Baum("a", 4);
+	AssoziativesArray liste3 = new Baum("a", 4);
+	AssoziativesArray liste4 = new Baum("a", 4);
+
+	@Test
+	public void putSizeTest() {
+		assertEquals(1, liste.size());
+		liste.put("ab", 3);
+		liste.put("d", 3);
+		liste.put("ei", 3);
+		liste.put("de", 3);
+		liste.put("hu", 3);
+		liste.put("kjhg", 3);
+		liste.put("hzu", 3);
+		liste.put("aum", 3);
+		liste.put("dzd", 3);
+		assertEquals(10, liste.size());
+
+		liste2.put("ab", 3);
+		assertEquals(2, liste2.size());
+		liste2.put("ab", 3);
+		assertEquals(2, liste2.size());
+	}
+
+	@Test
+	public void clearTest() {
+		liste3.clear();
+		assertEquals(0, liste3.size());
+	}
+
+	@Test
+	public void containsValueTest() {
+		liste.put("ab", 3);
+		liste.put("d", 3);
+		liste.put("ei", 3);
+		liste.put("de", 3);
+		liste.put("hu", 3);
+		liste.put("kjhg", 3);
+		liste.put("hzu", 3);
+		liste.put("aum", 3);
+		liste.put("dzd", 3);
+		assertEquals(true, liste.containsValue(3));
+		assertEquals(false, liste.containsValue(5));
+	}
+
+	@Test
+	public void continsKeyTest() {
+		liste.put("ab", 3);
+		liste.put("d", 3);
+		liste.put("ei", 3);
+		liste.put("de", 3);
+		liste.put("hu", 3);
+		liste.put("kjhg", 3);
+		liste.put("hzu", 3);
+		liste.put("aum", 3);
+		liste.put("dzd", 3);
+		assertEquals(true, liste.containsKey("dzd"));
+		assertEquals(false, liste.containsKey("AB"));
+	}
+
+	@Test
+	public void getTest() {
+		liste.put("ab", 3);
+		liste.put("d", 3);
+		liste.put("ei", 3);
+		liste.put("de", 3);
+		liste.put("hu", 3);
+		liste.put("kjhg", 3);
+		liste.put("hzu", 3);
+		liste.put("aum", 3);
+		liste.put("dzd", 3);
+		assertEquals(4, liste.get("a"));
+		assertEquals(3, liste.get("ei"));
+	}
+
+	@Test
+	public void isEmptyTest() {
+		assertEquals(false, liste.isEmpty());
+		assertEquals(true, liste3.isEmpty());
+	}
+
+	@Test
+	public void putAllTest() {
+	}
+
+	@Test
+	public void updateTest() {
+		assertEquals(4, liste.get("a"));
+		liste.update("a", 8);
+		assertEquals(8, liste.get("a"));
+	}
+
+	@Test
+	public void forEachTest() {
+	}
+
+	@Test
+	public void extractAllTest() {
+	}
+
+	@Test
+	public void mapTest() {
+	}
+	
+	@Test
+	public void toStringTest() {
+		liste4.put("b", 5);
+		liste4.put("bo", 5);
+		liste4.put("ac", 5);
+		liste4.put("af", 5);
+		liste4.put("ad", 5);
+		liste4.put("ba", 5);
+
+		assertEquals("{a=4, b=5, ac=5, ad=5, af=5, ba=5, bo=5}",
+				liste4.toString());
+	}
+}
