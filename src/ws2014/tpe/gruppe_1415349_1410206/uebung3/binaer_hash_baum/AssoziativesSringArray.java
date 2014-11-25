1fc6155c8b0b59b5798cff1de91226cb79a78f2e
Friederike <F.herbold@googlemail.com>
2014-11-25 17:27:32 +0100

    Assoziatives String Array fertig

---
+++ src/ws2014/tpe/gruppe_1415349_1410206/uebung3/binaer_hash_baum/AssoziativesSringArray.java
@@ -0,0 +1,9 @@
+package ws2014.tpe.gruppe_1415349_1410206.uebung3.binaer_hash_baum;
+
+public class AssoziativesSringArray<String,T> extends Baum<String,T> {
+
+	public AssoziativesSringArray(String schluessel , T wert) {
+		super(schluessel,wert);
+	}
+ 
+}
