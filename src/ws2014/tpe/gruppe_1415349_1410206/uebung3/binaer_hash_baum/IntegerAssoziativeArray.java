61a690a72c659d1145dbf7b4bb917362ea236694
Friederike <F.herbold@googlemail.com>
2014-11-25 17:20:46 +0100

    Assoziatives int Array erzeugt

---
+++ src/ws2014/tpe/gruppe_1415349_1410206/uebung3/binaer_hash_baum/IntegerAssoziativeArray.java
@@ -0,0 +1,7 @@
+package ws2014.tpe.gruppe_1415349_1410206.uebung3.binaer_hash_baum;
+
+public class IntegerAssoziativeArray<B,Integer> extends Baum<B,Integer> {
+	public IntegerAssoziativeArray(B schluessel , Integer wert) {
+		super(schluessel,wert);
+	}
+}
